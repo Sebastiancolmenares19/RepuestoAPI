6e08d2cd227e1baf03b6cc5b487bf8f791e53d05
root <root@DESKTOP-8P6H7RA.localdomain>
2026-04-25 02:01:30 -0400

    Mejoras dentro de main y database

--- main.py
+++ main.py
@@ -1,29 +1,29 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from exceptions import AlreadyExist, NotFound
-from models import UsuarioDB, RepuestoDB
 from database import engine, Base
 from routes.repuestos import router as repuestos_router
 from routes.usuarios import router as usuarios_router
-
+from routes.categorias import router as categorias_router
 
 app = FastAPI()
 Base.metadata.create_all(bind=engine)
 app.include_router(repuestos_router)
 app.include_router(usuarios_router)
+app.include_router(categorias_router)
 
 @app.exception_handler(AlreadyExist)
 async def user_already_exist_handler(request, exc):
     return JSONResponse(
         status_code= 409,
-        content={"detail": "Usuario ya existe"}
+        content={"detail": "Recurso ya existe"}
     )
 
 @app.exception_handler(NotFound)
-async def user_already_exist_handler(request, exc):
+async def not_found_handler(request, exc):
     return JSONResponse(
         status_code= 404,
-        content={"detail": "Usuario no encontrado"}
+        content={"detail": "Recurso no encontrado"}
         )
 
 
